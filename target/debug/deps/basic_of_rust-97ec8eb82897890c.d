4498ea7ed0ac3d07ae9540d926a85b02c2775b33
shogun-code <shoguncode93@gmail.com>
2023-07-05 22:33:51 +0700

    init

--- target/debug/deps/basic_of_rust-97ec8eb82897890c.d
+++
@@ -1,8 +0,0 @@
-/home/shogun/Rust-Bootcamp-2023/target/debug/deps/basic_of_rust-97ec8eb82897890c: src/lib.rs src/conditions.rs src/strings.rs src/functions.rs
-
-/home/shogun/Rust-Bootcamp-2023/target/debug/deps/basic_of_rust-97ec8eb82897890c.d: src/lib.rs src/conditions.rs src/strings.rs src/functions.rs
-
-src/lib.rs:
-src/conditions.rs:
-src/strings.rs:
-src/functions.rs:
--- target/debug/deps/basic_of_rust-c243f161b6e709b1.d
+++
@@ -1,8 +0,0 @@
-/home/shogun/Rust-Bootcamp-2023/target/debug/deps/basic_of_rust-c243f161b6e709b1.rmeta: src/lib.rs src/conditions.rs src/strings.rs src/functions.rs
-
-/home/shogun/Rust-Bootcamp-2023/target/debug/deps/basic_of_rust-c243f161b6e709b1.d: src/lib.rs src/conditions.rs src/strings.rs src/functions.rs
-
-src/lib.rs:
-src/conditions.rs:
-src/strings.rs:
-src/functions.rs:
