7791b7a12bd4682d21453f6e55758277443b1a06
shogun-code <shoguncode93@gmail.com>
2023-07-10 00:07:24 +0700

    add class-2-exercises

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,15 +4,25 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    if number > 0 {
+        "Positive"
+    } else if number < 0 {
+        "Negative"
+    } else {
+        "Zero"
+    }
 }
 
 // Exercise 3
@@ -22,8 +32,10 @@ fn check_number(number: u32) -> &'static str {
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
@@ -31,14 +43,26 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    if year % 4 == 0 {
+        if year % 100 == 0 {
+            year % 400 == 0
+        } else {
+            true
+        }
+    } else {
+        false
+    }
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
 }
 
 // Exercise 6
@@ -46,7 +70,22 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
+    if n <= 1 {
+        false
+    } else if n <= 3 {
+        true
+    } else if n % 2 == 0 || n % 3 == 0 {
+        false
+    } else {
+        let mut i = 5;
+        while i * i <= n {
+            if n % i == 0 || n % (i + 2) == 0 {
+                return false;
+            }
+            i += 6;
+        }
+        true
+    }
 }
 
 
