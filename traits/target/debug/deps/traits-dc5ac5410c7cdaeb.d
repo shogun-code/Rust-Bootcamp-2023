afc4158d6767a10727f64931b45e54aef5205fe5
shogun-code <shoguncode93@gmail.com>
2023-07-16 22:10:52 +0700

    update class-4-exercises

--- traits/target/debug/deps/traits-dc5ac5410c7cdaeb.d
+++
@@ -1,7 +0,0 @@
-/home/shogun/code/rust/VBI/tung/Rust-Bootcamp-2023/traits/target/debug/deps/traits-dc5ac5410c7cdaeb.rmeta: src/lib.rs
-
-/home/shogun/code/rust/VBI/tung/Rust-Bootcamp-2023/traits/target/debug/deps/libtraits-dc5ac5410c7cdaeb.rlib: src/lib.rs
-
-/home/shogun/code/rust/VBI/tung/Rust-Bootcamp-2023/traits/target/debug/deps/traits-dc5ac5410c7cdaeb.d: src/lib.rs
-
-src/lib.rs:
